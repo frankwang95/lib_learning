dac0cfaa90f453b28f55008c295a91d3a95446fc
Frank Wang <fkwang95@gmail.com>
2018-01-25 00:53:52 -0800

    Instatiated package setup.py. Reorganized into running and non-running code

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name='learning_lib',
+	version='0.0.0',
+	author='Frank Wang',
+	author_email='fkwang@uchicago.edu',
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=[
+		'numpy',
+		'tensorflow'
+	]
+)
