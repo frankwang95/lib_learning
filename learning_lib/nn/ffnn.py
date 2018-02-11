5d50251e07b2f325be92369ac8cc209e8c7918d1
Frank Wang <frankwang@frankwangdesign.com>
2018-02-11 14:40:30 -0800

    Added untested implementation of modular class-based reporting

--- learning_lib/nn/ffnn.py
+++ learning_lib/nn/ffnn.py
@@ -3,12 +3,12 @@ import numpy as np
 
 
 class FFNN(object):
-    def __init__(self, layer_config, post_proc_function=tf.identity, input_vector=None, session=None, float_type=tf.float32):
+    def __init__(self, layer_config, post_proc_function=None, loss_func=None, input_vector=None, session=None, float_type=None, monitors=[]):
         """ An implementation of a simple feed-forward neural network using the low-level
             tensorflow API.
 
         Inputs
-            layer_config <list(ints)>: A list containing dictionaries which describe the structure of the produced
+            layer_config <list>: A list containing dictionaries which describe the structure of the produced
                 network. The first element of the list should simply be a integer specifying the size of the input
                 dimension. The remaining items should be dictionaries each containing the following:
                 - n_nodes <int>: The number of nodes in the layer
@@ -22,6 +22,9 @@ class FFNN(object):
                 compute time but not traintime. One usecase is to apply softmax operations to the output layers in
                 light of the fact that cross-entropy/softmax computations are done in tandem to address issues with
                 numerical overflow.
+            loss_func <function(tf.Tensor, tf.Tensor -> tf.Tensor)>: A function that will compute a loss value from
+                the train inputs after passing through the network and the train outputs. The default is the tensorflow
+                implementation of mean squared loss.
             input_vector <tf.Tensor>: An (None, dim_in) shaped tensor used as the input layer to
                 the network. This can be used to link the network to more complex structures. If
                 not given, a tensorflow placeholder is initialized and used instead.
@@ -29,25 +32,38 @@ class FFNN(object):
                 option so that to prevent graph ownership issuses when an FFNN is used as a
                 component of a larger network. If not provided, a new interactive session will be
                 initialized.
+            float_type <tf.float32, tf.float64>: Gives the numerical type which internal values will be initalized to.
+            monitors <list(BaseMonitor)>: Gives a list of monitoring objects which will be tracked during training.
 
         Attributes
+            lc
             session
-            post_proc_function
-            learning_curve
             epochs
+            monitors
+
             input
-            output
             train_targets
-            activation
+            output
+            loss_val
+
+            float_type
             weights
             biases
+            activation
+            post_proc_function
         """
 
         # == Set basic attributes == #
-        self.post_proc_function = post_proc_function
-        self.learning_curve = []
-        self.loss_gradient = []
-        self.property = []
+        self.lc = lc
+        if post_proc_function is None:
+            self.post_proc_function = tf.identity
+        else:
+            self.post_proc_function = post_proc_function
+        if self.float_type is None:
+            self.float_type = tf.float32
+        else:
+            self.float_type = float_type
+        self.monitors = [m.link_to_network(self) for m in monitors]
         self.epochs = 0
 
         # == Define session == #
@@ -56,7 +72,7 @@ class FFNN(object):
         else:
             self.session = session
 
-        # == Define input and output points == #
+        # == Define input and output handles == #
         if input_vector is None:
             self.input = tf.placeholder(float_type, [None, layer_config[0]])
         else:
@@ -95,11 +111,18 @@ class FFNN(object):
         for w, b, a in zip(self.weights, self.biases, self.activation):
             self.output = a(tf.matmul(self.output, w) + b)
 
+        # == Define handles for accessing loss == #
+        if loss_func is None:
+            self.loss_func = tf.losses.mean_squared_error
+        else:
+            self.loss_func = loss_func
+        self.loss_val = self.loss_func(self.output, self.train_targets)
+
         # == Initialize variables == #
         self.session.run(tf.global_variables_initializer())
 
 
-    def train(self, train_in, train_out, loss_func=None, optimizer=None, batch_size=10, epochs=1, report_interval=10):
+    def train(self, train_in, train_out, optimizer=None, batch_size=10, epochs=1):
         """ Train the network weights with provided data. Trained weights can be accessed inside of
             the tensorflow session stored as `self.session`.
 
@@ -108,43 +131,26 @@ class FFNN(object):
                 input dimension.
             train_out <np.ndarray>: A [n, c] shaped array where n is the number of datapoints and c is size of the
                 output dimension.
-            loss_func <function(tf.Tensor, tf.Tensor -> tf.Tensor)>: A function that will compute a loss value from
-                the train inputs after passing through the network and the train outputs. The default is the tensorflow
-                implementation of mean squared loss.
             optimizer: A function which performs a optimization step. The default is the tensorflow Graident Descent
                 Optimizer with step size 0.1.
             batch_size <int>: The number of data points that will be passed through for each epoch. Default is 10.
             epochs <int>: The number of batches that will be passed through during the train job. Default is 1.
-            report_interval <int>: The number of epoches between which report metrics will be computed. Default is 10.
         """
-        if loss_func is None: loss_func = tf.losses.mean_squared_error
-        if optimizer is None: optimizer = tf.train.GradientDescentOptimizer(0.1)
 
-        loss_val = loss_func(self.output, self.train_targets)
-        train_step = optimizer.minimize(loss_val)
+        if optimizer is None: optimizer = tf.train.GradientDescentOptimizer(0.1)
+        train_step = optimizer.minimize(self.loss_val)
 
         for i in range(epochs):
             in_batch = np.roll(train_in, -batch_size * i, 0)[:batch_size]
             out_batch = np.roll(train_out, -batch_size * i, 0)[:batch_size]
             self.session.run(train_step, feed_dict={self.input: in_batch, self.train_targets: out_batch})
 
-            # Writing report information
-            if self.epochs % report_interval == 0:
-                self.loss_gradient.append((
-                    self.epochs,
-                    self.session.run(
-                        tf.gradients(loss_val, self.weights + self.biases),
-                        feed_dict={self.input: train_in, self.train_targets: train_out}
-                    )
-                ))
-                self.learning_curve.append((
-                    self.epochs,
-                    self.session.run(
-                        loss_val,
-                        feed_dict={self.input: train_in, self.train_targets: train_out}
-                    )
-                ))
-                print("Reached epoch {}".format(self.epochs))
+            # Generate reporting information
+            for m in self.monitors:
+                if m.check_update():
+                    m.evaluate()
+
+            print("Reached epoch {}".format(self.epochs))
             self.epochs += 1
 
 
---
+++ learning_lib/nn/monitoring/learning_curve.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from learning_lib.nn.monitoring.base_monitoring import BaseMonitor
+
+
+class LossMonitor(BaseMonitor):
+    def __init__(self, update_interval):
+        super().__init__(pd.DataFrame({'epochs': [], 'loss': []}), update_interval)
+
+    def evaluate(self, train_in, train_out):
+        loss_val = self.network.session.run(
+            self.network.loss_val,
+            feed_dict={self.input: train_in, self.train_targets: train_out}
+        )
+        self.values.append(
+            {'epochs': self.network.epochs, 'loss': loss_val},
+            ignore_index=True
+        )
