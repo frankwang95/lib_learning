c98beeb36fb2e33ac04b4641b28c49cdf2272483
frankwang95 <fkwang@uchicago.edu>
2019-03-17 14:30:37 -0700

    Made tables a class attribute.

--- lib_learning/collection/reverters/sql_reverter.py
+++ lib_learning/collection/reverters/sql_reverter.py
@@ -3,17 +3,18 @@ import MySQLdb as sql
 
 
 class SQLReverter(object):
-    def __init__(self, sql_params):
+    def __init__(self, sql_params, tables):
         self.sql_params = sql_params
+        self.tables = tables
 
 
-    def revert_fn(self, block, tables):
+    def revert_fn(self, block):
         removal_cutoff = datetime.utcfromtimestamp(int(block['_retrieval_datetime']))
 
         db = sql.connect(**self.sql_params)
         cur = db.cursor()
 
-        for table in tables:
+        for table in self.tables:
             cur.execute('DELETE FROM {} WHERE retrieval_datetime>="{}"'.format(table, removal_cutoff))
 
         db.commit()
