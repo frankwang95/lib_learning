24a2c13115d8fb0387c1aa08706ed3fcdbed11f9
frankwang95 <fkwang@uchicago.edu>
2019-08-03 16:44:00 -0700

    Removed compatability of reverters. Added ability for block generators to generate multiple batches per call

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='lib_learning',
-	version='0.0.0',
+	version='1.0.0',
 	author='Frank Wang',
 	author_email='fkwang95@gmail.com',
 	packages=find_packages(),
