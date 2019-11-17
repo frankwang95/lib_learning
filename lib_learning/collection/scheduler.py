88974a6169d327f07533515efbcb7f48269f4181
frankwang95 <fkwang@uchicago.edu>
2019-11-17 12:20:43 -0800

    Added scheduler documentation. Fixed retry logic.

--- lib_learning/collection/scheduler.py
+++ lib_learning/collection/scheduler.py
@@ -1,5 +1,3 @@
-
-
 import time
 import yaml
 import threading
@@ -15,12 +13,18 @@ class Scheduler(object):
         with workers (.workers.base_worker.Worker) via an interface (.interfaces.base_interface.Interface).
 
     Inputs:
-        scheduler_name
-        interface
-        block_generators
-        logger
-        task_timeout
-        confirm_interval
+        scheduler_name <str>: A name for the scheduler. Schedulers sharing the same interface must have unique names.
+        interface <Interface>: A interface object connecting this scheduler to a worker pool.
+        block_generators <BlockGenerator>: A block generator object to generate work blocks from user inputs.
+        logger <Logger>: A logger object to write scheduler logs to.
+        task_timeout <int>: A positive integer designating the number of seconds to wait before to timing out unfinished
+            work blocks.
+        confirm_interval <int>: A positive integer designating the length of the pause in seconds that the scheduler
+            waits between checking pending work blocks.
+        tries <int>: A postiive integer designating the default number of times a work block will be retried upon
+            failure. Work blocks can include a '_tries' key to overwrite this default value.
+        default_retry_delay <int>: A positive integer designating at least how long to wait before initiating a retry.
+            Work blocks can include a '_retry_delay' key to overwrite this default value.
     """
     def __init__(
         self, scheduler_name, interface, block_generator, logger, task_timeout=600, confirm_interval=20,
@@ -39,6 +43,8 @@ class Scheduler(object):
         self.global_lock = threading.Lock()
         # maps retrieval_time to iter reconstruction params
         self.pending_work = {}
+        # store of failed blocks for potential retry
+        self.failed_blocks = []
 
         thread = threading.Thread(target=self.pending_work_checker)
         thread.setDaemon(True)
@@ -57,6 +63,10 @@ class Scheduler(object):
                 generated_work = [generated_work]
 
             for block in generated_work:
+                assert '_retrieval_datetime' not in block
+                assert '_scheduler_name' not in block
+                assert '_status' not in block
+                assert '_finish_datetime' not in block
                 retrieval_datetime = time.time()
                 block = self.tag_block(block, retrieval_datetime)
                 self.pending_work[retrieval_datetime] = block
@@ -66,14 +76,10 @@ class Scheduler(object):
 
 
     def tag_block(self, block, retrieval_time):
-        assert '_retrieval_datetime' not in block
-        assert '_scheduler_name' not in block
-        assert '_status' not in block
-        assert '_finish_datetime' not in block
         block['_retrieval_datetime'] = retrieval_time
         block['_scheduler_name'] = self.scheduler_name
         block['_status'] = 'PENDING'
-        block['finish_datetime'] = None
+        block['_finish_datetime'] = None
         if '_tries' not in block:
             block['_tries'] = self.tries
         if '_retry_delay' not in block:
@@ -89,6 +95,23 @@ class Scheduler(object):
                 self.logger.info('checking for work status updates...')
                 self.check_confirmations()
                 self.check_timeouts()
+                self.process_failures()
+
+
+    def process_failures(self):
+        for block in self.failed_blocks:
+            old_rt = block['_retrieval_datetime']
+            if block['_tries'] <= 1:
+                self.logger.exception('block {} failed permanantly'.format(old_rt))
+                continue
+            elif time.time() - block['_finish_datetime'] > block['_retry_delay']:
+                new_rt = time.time()
+                block = tag_block(block, new_rt)
+                block['_tries'] -= 1
+                self.interface.push_work(block)
+                self.logger.warning('retrying block {} under ts {} with {} tries remaining'.format(
+                    old_rt, new_rt, block['_tries']
+                ))
 
 
     def check_confirmations(self):
@@ -100,17 +123,11 @@ class Scheduler(object):
 
             if block['_status'] == 'SUCCESS':
                 self.logger.info('block {} computation succeeded'.format(rt))
-            elif block['_tries'] > 1:
-                if time.time() - block['_finish_datetime'] > block['_retry_delay']:
-                    block['_status'] = 'PENDING'
-                    block['_finish_datetime'] = None
-                    block['_tries'] -= 1
-                    self.interface.push_work(block)
-                    self.logger.warning('block {} failed with {} tries remaining'.format(rt, block['_tries']))
             else:
-                self.logger.exception('block {} failed permanantly with exception\n{}'.format(rt, block['_status']))
+                self.failed_blocks.append(block)
+                self.logger.exception('block {} failed with exception\n{}'.format(rt, block['_status']))
 
-            if rt in self.pending_work: # handles case where job timeouts but then later is successful
+            if rt in self.pending_work:
                 del self.pending_work[rt]
 
 
@@ -119,4 +136,5 @@ class Scheduler(object):
         timeouts = [ts for ts in self.pending_work if ts < failure_cutoff]
         for ts in timeouts:
             self.logger.exception('block {} timed out'.format(ts))
+            self.failed_blocks.append(self.pending_work[ts])
             del self.pending_work[ts]
